9d5f86c514b4af0e6bb7af64d8f9d82ea4d8538d
nin-jin <nin-jin@users.noreply.github.com>
2022-10-10 07:43:16 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.js
+++ web.js
@@ -2881,7 +2881,7 @@ var $;
             return this.response(input, init).json();
         }
         static buffer(input, init) {
-            this.response(input, init).buffer();
+            return this.response(input, init).buffer();
         }
         static xml(input, init) {
             return this.response(input, init).xml();
