c5d279bd65b03bdbb651e0a1ddf3856d3e5bdd20
nin-jin <nin-jin@users.noreply.github.com>
2023-03-29 07:17:08 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.d.ts
+++ web.d.ts
@@ -54,6 +54,7 @@ declare namespace $ {
 declare namespace $ {
     class $mol_object2 {
         static $: typeof $$;
+        [Symbol.toStringTag]: string;
         [$mol_ambient_ref]: typeof $$;
         get $(): $;
         set $(next: $);
@@ -190,6 +191,7 @@ declare namespace $ {
         static plan_task: $mol_after_frame | null;
         static plan(): void;
         static sync(): void;
+        [Symbol.toStringTag]: string;
         cache: Result | Error | Promise<Result | Error>;
         get args(): Args;
         result(): Result | undefined;
@@ -197,8 +199,8 @@ declare namespace $ {
         constructor(id: string, task: (this: Host, ...args: Args) => Result, host?: Host | undefined, args?: Args);
         plan(): void;
         reap(): void;
-        toString(): any;
-        toJSON(): any;
+        toString(): string;
+        toJSON(): string;
         get $(): any;
         emit(quant?: $mol_wire_cursor): void;
         fresh(): void;
@@ -1108,6 +1110,85 @@ declare namespace $ {
 }
 
 declare namespace $ {
+    type $mol_data_value<Input = any, Output = any> = (val: Input) => Output;
+}
+
+declare namespace $ {
+    type $mol_type_equals<A, B> = (<X>() => X extends A ? 1 : 2) extends (<X>() => X extends B ? 1 : 2) ? unknown : never;
+}
+
+declare namespace $ {
+    type $mol_type_merge<Intersection> = Intersection extends (...a: any[]) => any ? Intersection : Intersection extends new (...a: any[]) => any ? Intersection : Intersection extends object ? $mol_type_merge_object<Intersection> extends Intersection ? unknown extends $mol_type_equals<$mol_type_merge_object<Intersection>, Intersection> ? Intersection : {
+        [Key in keyof Intersection]: $mol_type_merge<Intersection[Key]>;
+    } : Intersection : Intersection;
+    type $mol_type_merge_object<Intersection> = {
+        [Key in keyof Intersection]: Intersection[Key];
+    };
+}
+
+declare namespace $ {
+    type $mol_type_partial_undefined<Val> = $mol_type_merge<Partial<Val> & Pick<Val, {
+        [Field in keyof Val]: undefined extends Val[Field] ? never : Field;
+    }[keyof Val]>>;
+}
+
+declare namespace $ {
+    function $mol_data_setup<Value extends $mol_data_value, Config = never>(value: Value, config: Config): Value & {
+        config: Config;
+        Value: ReturnType<Value>;
+    };
+}
+
+declare namespace $ {
+    function $mol_data_record<Sub extends Record<string, $mol_data_value>>(sub: Sub): ((val: $mol_type_merge<Partial<{ [key in keyof Sub]: Parameters<Sub[key]>[0]; }> & Pick<{ [key in keyof Sub]: Parameters<Sub[key]>[0]; }, ({ [key in keyof Sub]: Parameters<Sub[key]>[0]; } extends infer T ? { [Field in keyof T]: undefined extends { [key in keyof Sub]: Parameters<Sub[key]>[0]; }[Field] ? never : Field; } : never)[keyof Sub]>>) => Readonly<$mol_type_merge<Partial<{ [key_1 in keyof Sub]: ReturnType<Sub[key_1]>; }> & Pick<{ [key_1 in keyof Sub]: ReturnType<Sub[key_1]>; }, ({ [key_1 in keyof Sub]: ReturnType<Sub[key_1]>; } extends infer T_1 ? { [Field_1 in keyof T_1]: undefined extends { [key_1 in keyof Sub]: ReturnType<Sub[key_1]>; }[Field_1] ? never : Field_1; } : never)[keyof Sub]>>>) & {
+        config: Sub;
+        Value: Readonly<$mol_type_merge<Partial<{ [key_1 in keyof Sub]: ReturnType<Sub[key_1]>; }> & Pick<{ [key_1 in keyof Sub]: ReturnType<Sub[key_1]>; }, ({ [key_1 in keyof Sub]: ReturnType<Sub[key_1]>; } extends infer T_2 ? { [Field_1 in keyof T_2]: undefined extends { [key_1 in keyof Sub]: ReturnType<Sub[key_1]>; }[Field_1] ? never : Field_1; } : never)[keyof Sub]>>>;
+    };
+}
+
+declare namespace $ {
+    function $mol_diff_path<Item>(...paths: Item[][]): {
+        prefix: Item[];
+        suffix: Item[][];
+    };
+}
+
+declare namespace $ {
+    class $mol_error_mix extends Error {
+        errors: Error[];
+        constructor(message: string, ...errors: Error[]);
+        toJSON(): string;
+    }
+}
+
+declare namespace $ {
+    class $mol_data_error extends $mol_error_mix {
+    }
+}
+
+declare namespace $ {
+    function $mol_data_array<Sub extends $mol_data_value>(sub: Sub): ((val: readonly Parameters<Sub>[0][]) => readonly ReturnType<Sub>[]) & {
+        config: Sub;
+        Value: readonly ReturnType<Sub>[];
+    };
+}
+
+declare namespace $ {
+    let $mol_data_string: (val: string) => string;
+}
+
+declare namespace $ {
+    function $mol_huggingface_run(this: $, space: string, method: string | number, ...data: readonly any[]): readonly string[];
+    function $mol_huggingface_async(space: string, method: number, ...data: readonly any[]): Promise<[string]> & {
+        destructor: () => void;
+    };
+}
+
+declare namespace $ {
+    function $hyoo_lingua_translate(this: $, lang: string, text: string): string;
+}
+
+declare namespace $ {
     interface $mol_locale_dict {
         [key: string]: string;
     }
@@ -1116,7 +1197,7 @@ declare namespace $ {
         static lang(next?: string): string;
         static source(lang: string): any;
         static texts(lang: string, next?: $mol_locale_dict): $mol_locale_dict;
-        static text(key: string): string;
+        static text(key: string): {} | null;
         static warn(key: string): null;
     }
 }
@@ -1796,19 +1877,6 @@ declare namespace $ {
 }
 
 declare namespace $ {
-    type $mol_type_equals<A, B> = (<X>() => X extends A ? 1 : 2) extends (<X>() => X extends B ? 1 : 2) ? unknown : never;
-}
-
-declare namespace $ {
-    type $mol_type_merge<Intersection> = Intersection extends (...a: any[]) => any ? Intersection : Intersection extends new (...a: any[]) => any ? Intersection : Intersection extends object ? $mol_type_merge_object<Intersection> extends Intersection ? unknown extends $mol_type_equals<$mol_type_merge_object<Intersection>, Intersection> ? Intersection : {
-        [Key in keyof Intersection]: $mol_type_merge<Intersection[Key]>;
-    } : Intersection : Intersection;
-    type $mol_type_merge_object<Intersection> = {
-        [Key in keyof Intersection]: Intersection[Key];
-    };
-}
-
-declare namespace $ {
     type $mol_type_intersect<Union> = (Union extends any ? (_: Union) => void : never) extends ((_: infer Intersection) => void) ? Intersection : never;
 }
 
@@ -1922,7 +1990,7 @@ declare namespace $ {
         nav_focused(component?: any): any;
         Nav(): $$.$mol_nav;
         suggests_showed(val?: any): boolean;
-        hint(): string;
+        hint(): {} | null;
         submit(event?: any): any;
         enabled(): boolean;
         keyboard(): string;
@@ -1966,7 +2034,7 @@ declare namespace $ {
 
 declare namespace $ {
     class $mol_link_source extends $mol_link {
-        hint(): string;
+        hint(): {} | null;
         sub(): readonly any[];
         Icon(): $mol_icon_github_circle;
     }
@@ -2031,10 +2099,10 @@ declare namespace $ {
         Toy_image(id: any): $mol_image;
         toy_price(id: any): $mol_unit_money;
         Toy_price(id: any): $$.$mol_cost;
-        toy_size_prefix(): string;
+        toy_size_prefix(): {} | null;
         toy_size(id: any): string;
         Toy_size(id: any): $mol_view;
-        toy_reviews_prefix(): string;
+        toy_reviews_prefix(): {} | null;
         toy_reviews(id: any): number;
         Toy_reviews(id: any): $mol_view;
         Toy_info(id: any): $mol_view;
@@ -2139,17 +2207,17 @@ declare namespace $ {
         photo_filter(): string;
         Photo(): $mol_image;
         Image(): $mol_view;
-        buy(): string;
+        buy(): {} | null;
         price(): $mol_unit_money;
         Price(): $$.$mol_cost;
         Buy_button(): $mol_button_major;
         Buy_link(): $$.$mol_link;
         type(): string;
         Type(): $mol_view;
-        size_prefix(): string;
+        size_prefix(): {} | null;
         size(): string;
         Size(): $mol_view;
-        reviews_prefix(): string;
+        reviews_prefix(): {} | null;
         reviews(): number;
         Reviews(): $mol_view;
         Parameters(): $mol_view;
@@ -2260,28 +2328,28 @@ declare namespace $ {
         search(event?: any): any;
         Hotkey(): $$.$mol_hotkey;
         filter_type(val?: any): string;
-        filter_type_option_all(): string;
+        filter_type_option_all(): {} | null;
         filter_type_options(): {
-            all: string;
+            all: {} | null;
         };
         Filter_type(): $$.$mol_switch;
         filter_size(val?: any): string;
-        filter_size_option_all(): string;
+        filter_size_option_all(): {} | null;
         filter_size_options(): {
-            all: string;
+            all: {} | null;
         };
         Filter_size(): $$.$mol_switch;
         filter_popular(val?: any): boolean;
-        filter_popular_title(): string;
+        filter_popular_title(): {} | null;
         Filter_popular(): $$.$mol_check;
         sort_key(val?: any): string;
-        sort_option_price(): string;
-        sort_option_size(): string;
-        sort_option_title(): string;
+        sort_option_price(): {} | null;
+        sort_option_size(): {} | null;
+        sort_option_title(): {} | null;
         sort_items(): {
-            price: string;
-            size: string;
-            title: string;
+            price: {} | null;
+            size: {} | null;
+            title: {} | null;
         };
         Sort(): $$.$mol_switch;
         toys(): readonly any[];
@@ -2305,11 +2373,11 @@ declare namespace $.$$ {
         search(event: KeyboardEvent): void;
         filter_type(next?: string): string;
         filter_type_options(): {
-            all: string;
+            all: {} | null;
         };
         filter_size(next?: string): string;
         filter_size_options(): {
-            all: string;
+            all: {} | null;
         };
         filter_popular(next?: boolean): boolean;
         sort_key(next?: string): string;
