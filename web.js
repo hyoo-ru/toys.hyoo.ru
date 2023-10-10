c5174db45c0faf172adae96c0d81b307f052416a
nin-jin <nin-jin@users.noreply.github.com>
2023-10-10 07:15:45 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.js
+++ web.js
@@ -4103,6 +4103,15 @@ var $;
         dom_name() {
             return "a";
         }
+        uri_off() {
+            return "";
+        }
+        uri_native() {
+            return null;
+        }
+        external() {
+            return false;
+        }
         attr() {
             return {
                 ...super.attr(),
