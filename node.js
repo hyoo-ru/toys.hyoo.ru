cb0830ef3203c242da6af03c4378c67470d5aa41
nin-jin <nin-jin@users.noreply.github.com>
2025-09-09 07:21:19 +0000

    Deploying to gh-pages from @ hyoo-ru/toys.hyoo.ru@3bf85236471110a206bc1f9d4829746d3adf3833 🚀

--- node.js
+++ node.js
@@ -6711,6 +6711,7 @@ var $;
 			return {
 				...(super.attr()), 
 				"src": (this.uri()), 
+				"title": (this.hint()), 
 				"alt": (this.title()), 
 				"loading": (this.loading()), 
 				"decoding": (this.decoding()), 
