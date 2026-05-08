e66a45b9f1d0dbde8f1d292ccc13a0dc8c61b4ed
nin-jin <nin-jin@users.noreply.github.com>
2026-05-08 08:22:08 +0000

    Deploying to gh-pages from @ hyoo-ru/toys.hyoo.ru@3bf85236471110a206bc1f9d4829746d3adf3833 🚀

--- web.js
+++ web.js
@@ -5961,6 +5961,7 @@ var $;
             render_visible_only() {
                 return this.$.$mol_support_css_overflow_anchor();
             }
+            _view_window_last = [0, 0];
             view_window(next) {
                 const kids = this.sub();
                 if (kids.length < 3)
@@ -5970,7 +5971,7 @@ var $;
                 const rect = this.view_rect();
                 if (next)
                     return next;
-                let [min, max] = $mol_mem_cached(() => this.view_window()) ?? [0, 0];
+                let [min, max] = $mol_mem_cached(() => this.view_window()) ?? this._view_window_last;
                 const shift = this.view_window_shift();
                 this.view_window_shift(0);
                 min += shift;
@@ -6065,7 +6066,7 @@ var $;
             sub_visible() {
                 return [
                     ...this.gap_before() ? [this.Gap_before()] : [],
-                    ...this.sub().slice(...this.view_window()),
+                    ...this.sub().slice(...this._view_window_last = this.view_window()),
                     ...this.gap_after() ? [this.Gap_after()] : [],
                 ];
             }
